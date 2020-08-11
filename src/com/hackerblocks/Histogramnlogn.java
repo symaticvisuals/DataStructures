575e8cdcce537187ca9c6e55e28f8cf70ff8a7e9
symaticvisuals <symaticvisuals02@gmail.com>
2020-08-11 07:19:13 +0530

    new

--- src/com/hackerblocks/Histogramnlogn.java
+++ src/com/hackerblocks/Histogramnlogn.java
@@ -6,13 +6,13 @@ public class Histogramnlogn {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int ar[] = new int[n];
+        long ar[] = new long[n];
         for (int i = 0; i <ar.length ; i++) {
-            ar[i] = s.nextInt();
+            ar[i] = s.nextLong();
         }
         System.out.println(Maxarea(ar,0,ar.length-1));
     }
-    public static int Maxarea(int arr[],int start,int end )
+    public static long Maxarea(long arr[],int start,int end )
     {
         if(start==end)
         {
@@ -22,7 +22,7 @@ public class Histogramnlogn {
         {
             return 0;
         }
-        int min_value=Integer.MAX_VALUE;
+        long min_value=Long.MAX_VALUE;
         int min = -1;
         for (int i = start; i <=end ; i++) {
             if(arr[i]<min_value)
@@ -31,9 +31,9 @@ public class Histogramnlogn {
                 min =i;
             }
         }
-        int maxwidtharea = min_value*(end-start+1);
-        int lma = Maxarea(arr,start,min-1);
-        int rma = Maxarea(arr,min+1,end);
+        long maxwidtharea = min_value*(end-start+1);
+        long lma = Maxarea(arr,start,min-1);
+        long rma = Maxarea(arr,min+1,end);
         return Math.max(maxwidtharea,Math.max(lma,rma));
     }
 
